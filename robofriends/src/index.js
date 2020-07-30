934c6063d48fb2c10260a3497a8c49c16aa7b250
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-29 23:11:36 -0500

    passed the properties into it and made everything text centered

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -6,6 +6,7 @@ import Card from './Card.js'
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 import 'tachyons' //similar to bootstrap
+import { robots } from './robots'
 
 
 // ReactDOM.render(
@@ -23,9 +24,9 @@ import 'tachyons' //similar to bootstrap
 
 ReactDOM.render(
   <div> 
-      <Card />
-      <Card />
-      <Card />
+      <Card id={robots[0].id} name= {robots[0].name} email = {robots[0].email}/>
+      <Card id={robots[1].id} name= {robots[1].name} email = {robots[1].email}/>
+      <Card id={robots[2].id} name= {robots[2].name} email = {robots[2].email}/>
   </div>
   
   ,
