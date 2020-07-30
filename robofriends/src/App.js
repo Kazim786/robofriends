cd664883336e2cfd6b379b7328a40beef0463bd3
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-30 13:29:25 -0500

    made search box component

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import CardList from './CardList'
 import './App.css';
 import { robots } from './robots'
-
+import SearchBox from './SearchBox'
 function App() {
 return(
-    <CardList robots = {robots} />
-
+    <div>
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots = {robots} />
+    </div>
 )
 
 }
