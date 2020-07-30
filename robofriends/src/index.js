8c9e047e8e07aa8d4f7fbefbdd05d547dc0a8c4a
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-30 13:24:52 -0500

    putting everything in the App component

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import Hello from './Hello.js';
 
-import CardList from './CardList'
-// import App from './App';
+import App from './App';
 // import * as serviceWorker from './serviceWorker';
 import 'tachyons' //similar to bootstrap
-import { robots } from './robots'
 
 
 // ReactDOM.render(
@@ -24,7 +22,7 @@ import { robots } from './robots'
 // );
 
 ReactDOM.render(
-  <CardList robots = {robots} />
+  <App />
   
   ,
   document.getElementById('root')
