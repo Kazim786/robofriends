baad89a0b159cfe619ca411ae1445c014740afd0
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-29 23:47:36 -0500

    to dynamically create many robots I used the map array method

--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Card from './Card.js'
-const CardList = () => {
+import { robots } from './robots'
+
+
+const CardList = ({ robots }) => {
+    const cardComponent = robots.map((user, i) => {
+    return  <Card id={robots[i].id} name= {robots[i].name} email = {robots[i].email}/>
+    })
     return (
     <div> 
         <Card id={robots[0].id} name= {robots[0].name} email = {robots[0].email}/>
@@ -11,3 +17,4 @@ const CardList = () => {
 }
 
 export default CardList
+
