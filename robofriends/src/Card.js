13390155c43984ee8ab758cb8a2c0da973b462db
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-29 23:14:16 -0500

    made it dynamic so that the pictures are all different

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     return (
         
         <div className = 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt = "Robots" src= "https://robohash.org/test?200x200" />
+            <img alt = "Robots" src= {`https://robohash.org/${props.id}`}/>
                 <div>
                     <h2>{props.name}</h2>
                     <p>{props.email}</p>
