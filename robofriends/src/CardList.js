6403bd703bd0da1426c435761b89aad2a849b4b9
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-29 23:54:15 -0500

    debugged

--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -5,8 +5,13 @@ import { robots } from './robots'
 
 const CardList = ({ robots }) => {
     const cardComponent = robots.map((user, i) => {
-    return  <Card key = {i} id={robots[i].id} name= {robots[i].name} email = {robots[i].email}/>
-    })
+    return (
+    <Card key = {i} 
+    id={robots[i].id} 
+    name= {robots[i].name} 
+    email = {robots[i].email}/>
+    )})
+
     return (
     <div> 
         {cardComponent}
