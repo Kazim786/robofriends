9ebeb0902446f7773004b7e0505d49efeed6013d
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-29 23:40:27 -0500

    made a cardlist component

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const Card = (props) => {
+    const {name, email, id} = props
     return (
         
         <div className = 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt = "Robots" src= {`https://robohash.org/${props.id}`}/>
+            <img alt = "Robots" src= {`https://robohash.org/${id}`}/>
                 <div>
-                    <h2>{props.name}</h2>
-                    <p>{props.email}</p>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
                 </div>
         </div>
     )
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import Hello from './Hello.js';
-import Card from './Card.js'
+
+import CardList from './CardList'
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 import 'tachyons' //similar to bootstrap
@@ -23,11 +24,7 @@ import { robots } from './robots'
 // );
 
 ReactDOM.render(
-  <div> 
-      <Card id={robots[0].id} name= {robots[0].name} email = {robots[0].email}/>
-      <Card id={robots[1].id} name= {robots[1].name} email = {robots[1].email}/>
-      <Card id={robots[2].id} name= {robots[2].name} email = {robots[2].email}/>
-  </div>
+  <CardList robots = {robots} />
   
   ,
   document.getElementById('root')
