b15aa345d9f5a33cf1ae7e4a19b91a65751f05f1
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-29 20:19:37 -0500

    made component 1

---
+++ robofriends/src/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hello(){
+    return (
+    <div className= 'Hello'>
+        <h1>Hi i am your first component</h1>
+
+    </div>
+
+)}
+
+export default Hello;
