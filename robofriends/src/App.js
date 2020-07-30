4126fc20ef41c87742d5314cc9ae72a2812f22ba
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-30 14:03:02 -0500

    State

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -3,15 +3,28 @@ import CardList from './CardList'
 import './App.css';
 import { robots } from './robots'
 import SearchBox from './SearchBox'
-function App() {
-return(
-    <div>
-        <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots = {robots} />
-    </div>
-)
+import { render } from 'react-dom';
 
-}
+
+
+
+class App extends Component {
+    constructor(){
+        this.state = {
+            robots: robots,
+            searchfield = ''
+        }
+    }
+    render(){
+        return(
+            <div className = "tc">
+            <h1>RoboFriends</h1>
+            <SearchBox />
+            <CardList robots = {robots} />
+            </div>
+            )
+        }
+
+    }
 
 export default App;
